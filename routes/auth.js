26b2bea1f2129395f2afe3d868ee703f256e4de9
musfirotus <musfirotus@gmail.com>
2020-08-31 18:29:51 +0700

    unused controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const AuthController = require("../controllers/AuthController")
 const UserController = require('../controllers/UserController')
-// const auth = require('../middleware/AuthMiddleware')
 
 router.post('/signup', UserController.saveUser)
 router.post('/login', AuthController.login)
