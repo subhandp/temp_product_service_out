e259f624bda00b9c21adbdd00079ef05b7d769d4
musfirotus <musfirotus@gmail.com>
2020-09-01 10:52:14 +0700

    auth bug fixed

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/AuthMiddleware')
 
 const UserController = require('../controllers/UserController')
 
 router
-  .get('/home', auth, UserController.home)
-  .get('/', auth, UserController.getUsers)
-  .get('/:id', auth, UserController.getUser)
-  .post('/', auth, UserController.saveUser)
-  .put('/:id', auth, UserController.updateUser)
-  .delete('/:id', auth, UserController.deleteUser)
+  .get('/home', UserController.home)
+  .get('/', UserController.getUsers)
+  .get('/:id', UserController.getUser)
+  .post('/', UserController.saveUser)
+  .put('/:id', UserController.updateUser)
+  .delete('/:id', UserController.deleteUser)
 
 module.exports = router
