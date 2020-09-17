4553122afbb91f49aa901d76a50107a46b32010a
Husen Efendi <husenef@gmail.com>
2020-09-17 17:05:57 +0700

    update config to pg

--- config/config.js
+++ config/config.js
@@ -1,25 +1,63 @@
 require("dotenv").config();
 
+// module.exports = {
+//   "development": {
+//     "username": process.env.DB_USERNAME,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_NAME,
+//     "host": process.env.DB_HOST,
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  development: {
+    use_env_variable: process.env.DB_PATH,
+    dialect: process.env.DB_DIALECT,
+    operatorAliases: 0,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      timestamps: 1,
+    },
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    use_env_variable: process.env.DB_PATH,
+    dialect: process.env.DB_DIALECT,
+    operatorAliases: 0,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      timestamps: 1,
+    },
+  },
+};
