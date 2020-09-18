e7d54ab5a098167c3b1d6efcd4ac2748004a9807
Husen Efendi <husenef@gmail.com>
2020-09-18 19:28:34 +0700

    update cors

--- app.js
+++ app.js
@@ -26,21 +26,7 @@ const allowedOrigins = [
 //cors
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      allowedOrigins.map((e) => {
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg =
-            "The CORS policy for this site does not " +
-            "allow access from the specified Origin.";
-          return callback(new Error(msg), false);
-        }
-      });
-
-      return callback(null, true);
-    },
+    origin: true
   })
 );
 // untuk cloudinary
