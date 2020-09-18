98808c203d9f08e153719e2aed4848b8d7d161e1
Husen Efendi <husenef@gmail.com>
2020-09-18 18:57:42 +0700

    update cors

--- app.js
+++ app.js
@@ -18,8 +18,24 @@ const reportRoute = require("./routes/print");
 const auth = require("./middleware/AuthMiddleware");
 const taskScheduler = require("./helpers/taskScheduler");
 
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
 //cors
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 // untuk cloudinary
 app.use(
   fileUpload({
