2f6d922a2c5cc3d4cb1a6e78fb7e1088ae615522
Husen Efendi <husenef@gmail.com>
2020-09-19 10:49:17 +0700

    update cors for heroku

--- app.js
+++ app.js
@@ -40,11 +40,11 @@ const whitelist = [
 // );
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // untuk cloudinary
 app.use(
